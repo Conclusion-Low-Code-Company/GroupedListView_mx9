b0ca2a4ac4ae1f3c95c5a5476cb26e9f769fb147
Bart Heijs <bheijs@conclusion.nl>
2023-02-12 14:19:59 +0100

    minor fixes + comments added

--- src/components/GroupedList.tsx
+++ src/components/GroupedList.tsx
@@ -6,14 +6,17 @@ import ListItem from "./ListItem";
 import { GroupedListViewContainerProps } from "../../typings/GroupedListViewProps";
 
 export default function GroupedList(props: GroupedListViewContainerProps): ReactElement {
+    // Keep track of the last known group ID
     let lastKnownGroupId = "";
 
     return (
         <div style={props.style} className={"glv mx-listview " + props.class}>
             <ul className="glv__list">
                 {props.inputList.items!.map(item => {
-                    // if (props.groupId(item).value !== lastKnownGroupId)
+                    // Map over the list items and render each item
                     if (props.groupId.get(item).value !== lastKnownGroupId) {
+                        // If the group ID of the current item is different from the last known group ID,
+                        // update the last known group ID and render a new group header and the item
                         lastKnownGroupId = props.groupId.get(item).value!;
 
                         return (
@@ -33,6 +36,7 @@ export default function GroupedList(props: GroupedListViewContainerProps): React
                             </Fragment>
                         );
                     }
+                    // If the group ID is the same as the last known group ID, just render the item
                     return (
                         <ListItem widget={props.widget} item={item} listItemClick={props.listItemClick} key={item.id} />
                     );
